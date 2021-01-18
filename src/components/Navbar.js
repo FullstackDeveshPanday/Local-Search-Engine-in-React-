1d700f64f63c7040a0ee33f06be207fea8bcd29c
FullstackDeveshPanday <deveshpandeyy007@gmail.com>
2021-01-18 18:21:40 +0530

    new commit -6

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ export const Navbar = () => {
     return (
         <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-                <span className="text-white text-center"><h2>Mobile Duniya</h2></span>
+                <span className="navbar-brand text-white text-center"><h2>Mobile Duniya</h2></span>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                     </button>
